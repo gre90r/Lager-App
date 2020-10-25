7e617b5defe1d6f33290b9622f999be6dfecdff8
gre90r <gregor_hoben@live.de>
2020-10-25 18:09:09 +0100

    added browser search page prototype

---
+++ src/client/browser/lager-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MenubarComponent } from './menubar/menubar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    MenubarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
